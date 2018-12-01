20233510beea8ad6049c05f9bb5c948084bb8b5e
Themen Danielson <tsdanielson@sep.com>
2018-12-01 13:40:20 -0500

    What a mess; Day01

---
+++ Day01/Puzzle.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
+let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+
+var lines: [Int] = []
+content.enumerateLines { line, _ in
+    lines.append(Int(line)!)
+}
+var found = false
+var answer = 0
+var counts : Set<Int> = []
+var count = 0
+while (!found){
+    for line in lines {
+//        let number = Int(line)
+        count = count + line
+        if counts.contains(count) && found == false {
+            answer = count
+            found = true
+        } else {
+            counts.insert(count)
+        }
+    }
+}
+print(answer)
