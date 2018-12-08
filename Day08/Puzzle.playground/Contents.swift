35a12d7544aeb932d588137d4782e7d6a6d7864f
Themen Danielson <tsdanielson@sep.com>
2018-12-08 13:51:04 -0500

    Day 8

---
+++ Day08/Puzzle.playground/Contents.swift
@@ -0,0 +1,97 @@
+import UIKit
+
+let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
+let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+
+class Node : CustomStringConvertible {
+    let id: Int
+    var nodeIds : [Int]
+    var metas : [Int]
+    
+    var description: String {
+        return "\(id) has \(metas) with nodes: \(nodeIds)"
+    }
+    
+    init(id: Int, nodeIds: [Int], metas: [Int]) {
+        self.id = id
+        self.nodeIds = nodeIds
+        self.metas = metas
+    }
+    
+    func value(nodes: [Node]) -> Int {
+        print("get value for \(id)")
+        var sum = 0
+        if nodeIds.count == 0 {
+            for meta in metas {
+                sum += meta
+            }
+            print("value for \(id) is \(sum)")
+            return sum
+        } else {
+            for meta in metas {
+                if meta > 0 && meta <= nodeIds.count {
+                    let nodeNumber = nodeIds[meta - 1]
+                    let node = nodes.first{$0.id == nodeNumber}
+                    sum += node!.value(nodes: nodes)
+                }
+            }
+            return sum
+        }
+    }
+}
+
+var values: [Int] = []
+
+let items = content.components(separatedBy: " ")
+print(items.count)
+for item in items {
+    if let value = Int(item) {
+        values.append(value)
+    }
+}
+
+print(values)
+
+var id = 0
+func createNode() -> [Node] {
+    var nodes : [Node] = []
+    var myNodes : [Int] = []
+    var metas : [Int] = []
+    let myid = id
+    id += 1
+    let nodeCount = values.remove(at: 0)
+    let metaCount = values.remove(at: 0)
+    
+    if nodeCount > 0 {
+        for _ in 0..<nodeCount {
+            let newNodes = createNode()
+            myNodes.append(newNodes.last!.id)
+            nodes.append(contentsOf: newNodes)
+        }
+    }
+
+    if metaCount > 0 {
+        for _ in 0..<metaCount {
+            metas.append(values.remove(at: 0))
+        }
+    }
+    
+    let node = Node(id: myid, nodeIds: myNodes, metas: metas)
+    print(node)
+    nodes.append(node)
+    
+    return nodes
+}
+
+var nodes = createNode()
+//var sum = 0
+//while (nodes.count > 0) {
+//    let node = nodes.remove(at: nodes.count-1)
+//    for meta in node.metas {
+//        sum += meta
+//    }
+////    print(nodes.remove(at: nodes.count-1))
+//}
+//print(sum)
+
+print (nodes[nodes.count-1].value(nodes: nodes))
