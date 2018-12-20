ec7bfab054db99d0905ae7c924270e5620519ec6
Themen Danielson <tsdanielson@sep.com>
2018-12-20 16:58:58 -0500

    with debug and dictionary

--- Day20/Day01/Day01/main.swift
+++ Day20/Day01/Day01/main.swift
@@ -6,8 +6,6 @@ let url = URL(fileURLWithPath: "input.txt", relativeTo: currentDirectoryURL)
 var content = try String(contentsOf: url, encoding: String.Encoding.utf8)
 
 class Room : CustomStringConvertible {
-    static var iders = 0
-    let id: Int
     var x : Int
     var y : Int
     var distance = Int.max
@@ -18,19 +16,17 @@ class Room : CustomStringConvertible {
     
     var description: String {
         let doors = [n != nil, e != nil, s != nil, w != nil]
-        return "(\(x), \(y)) with \(doors)"
+        return "(\(x), \(y)) with \(doors) at distance \(distance)"
     }
     
     init(x: Int, y: Int) {
-        self.id = Room.iders
-        Room.iders += 1
         self.x = x
         self.y = y
     }
 }
 
 var startRoom = Room(x: 0, y: 0)
-var rooms = [startRoom]
+var rooms : [String : Room] = ["0 0" : startRoom]
 var splitCount = 0
 func parse(cRoom: Room, path: String) {
     if path == "" { return }
@@ -42,41 +38,39 @@ func parse(cRoom: Room, path: String) {
         var paths = [String]()
         var deep = 0
         var cPath = ""
-        var finalPath = ""
-        for char in nextPath {
-            if deep < 0 {
-                finalPath += String(char)
-            } else {
-                switch char {
-                case "(":
-                    deep += 1
-                    cPath += String(char)
-                case ")":
-                    if deep == 0 {
-                        paths.append(cPath)
-                    } else {
-                        cPath += String(char)
+        for (index, char) in nextPath.enumerated() {
+            switch char {
+            case "(":
+                deep += 1
+                cPath += String(char)
+            case ")":
+                if deep == 0 {
+                    paths.append(cPath)
+                    let startIndex = nextPath.index(nextPath.startIndex, offsetBy: index + 1)
+                    let finalPath = nextPath[startIndex..<nextPath.endIndex]
+                    print("\nSplit \(splitCount) with \(finalPath.count) character after")
+                    print(finalPath)
+                    for path in paths {
+                        parse(cRoom: cRoom, path: "\(path)\(finalPath)")
                     }
-                    deep -= 1
-                case "|":
-                    if deep == 0 {
-                        paths.append(cPath)
-                        cPath = ""
-                    } else {
-                        cPath += String(char)
-                    }
-                default:
+                    return
+                } else {
                     cPath += String(char)
                 }
+                deep -= 1
+            case "|":
+                if deep == 0 {
+                    paths.append(cPath)
+                    cPath = ""
+                } else {
+                    cPath += String(char)
+                }
+            default:
+                cPath += String(char)
             }
         }
-        print("Split \(splitCount) with \(finalPath.count) character after")
-        for path in paths {
-            parse(cRoom: cRoom, path: "\(path)\(finalPath)")
-        }
-        return
     case "N":
-        if let nRoom = rooms.first(where: {$0.x == cRoom.x && $0.y == cRoom.y+1}) {
+        if let nRoom = rooms["\(cRoom.x) \(cRoom.y+1)"]  { // rooms.first(where: {$0.x == cRoom.x && $0.y == cRoom.y+1}) {
             cRoom.n = nRoom
             nRoom.s = cRoom
             parse(cRoom: nRoom, path: nextPath)
@@ -84,11 +78,11 @@ func parse(cRoom: Room, path: String) {
             let nRoom = Room(x: cRoom.x, y: cRoom.y+1)
             cRoom.n = nRoom
             nRoom.s = cRoom
-            rooms.append(nRoom)
+            rooms["\(cRoom.x) \(cRoom.y+1)"] = nRoom
             parse(cRoom: nRoom, path: nextPath)
         }
     case "E":
-        if let nRoom = rooms.first(where: {$0.x == cRoom.x+1 && $0.y == cRoom.y})  {
+        if let nRoom = rooms["\(cRoom.x+1) \(cRoom.y)"]  { // rooms.first(where: {$0.x == cRoom.x+1 && $0.y == cRoom.y})  {
             cRoom.e = nRoom
             nRoom.w = cRoom
             parse(cRoom: nRoom, path: nextPath)
@@ -96,11 +90,11 @@ func parse(cRoom: Room, path: String) {
             let nRoom = Room(x: cRoom.x+1, y: cRoom.y)
             cRoom.e = nRoom
             nRoom.w = cRoom
-            rooms.append(nRoom)
+            rooms["\(cRoom.x+1) \(cRoom.y)"] = nRoom
             parse(cRoom: nRoom, path: nextPath)
         }
     case "S":
-        if let nRoom = rooms.first(where: {$0.x == cRoom.x && $0.y == cRoom.y-1})  {
+        if let nRoom = rooms["\(cRoom.x) \(cRoom.y-1)"]  { // rooms.first(where: {$0.x == cRoom.x && $0.y == cRoom.y-1})  {
             cRoom.s = nRoom
             nRoom.n = cRoom
             parse(cRoom: nRoom, path: nextPath)
@@ -108,11 +102,11 @@ func parse(cRoom: Room, path: String) {
             let nRoom = Room(x: cRoom.x, y: cRoom.y-1)
             cRoom.s = nRoom
             nRoom.n = cRoom
-            rooms.append(nRoom)
+            rooms["\(cRoom.x) \(cRoom.y-1)"] = nRoom
             parse(cRoom: nRoom, path: nextPath)
         }
     case "W":
-        if let nRoom = rooms.first(where: {$0.x == cRoom.x-1 && $0.y == cRoom.y})  {
+        if let nRoom = rooms["\(cRoom.x-1) \(cRoom.y)"]  { // rooms.first(where: {$0.x == cRoom.x-1 && $0.y == cRoom.y})  {
             cRoom.w = nRoom
             nRoom.e = cRoom
             parse(cRoom: nRoom, path: nextPath)
@@ -120,14 +114,13 @@ func parse(cRoom: Room, path: String) {
             let nRoom = Room(x: cRoom.x-1, y: cRoom.y)
             cRoom.w = nRoom
             nRoom.e = cRoom
-            rooms.append(nRoom)
+            rooms["\(cRoom.x-1) \(cRoom.y)"] = nRoom
             parse(cRoom: nRoom, path: nextPath)
         }
     default:
         break
     }
 }
-print("Found \(rooms.count) rooms")
 func findDistances(cRoom: Room, steps: Int = 1) {
     var rooms = [Room]()
     for r in [cRoom.n, cRoom.e, cRoom.s, cRoom.w] {
@@ -142,40 +135,41 @@ func findDistances(cRoom: Room, steps: Int = 1) {
 }
 
 parse(cRoom: startRoom, path: String(content.dropFirst().dropLast()))
+print("Found \(rooms.count) rooms")
+
 startRoom.distance = 0
 findDistances(cRoom: startRoom)
-
-rooms.sort { (first, second) -> Bool in
-    first.distance > second.distance
+var result = rooms.sorted { (arg1, arg2) -> Bool in
+    arg1.value.distance > arg2.value.distance
 }
+print(result.first!.value)
 
-print(rooms.first!)
 
-//rooms.sort { (first, second) -> Bool in
-//    if first.y == second.y {
-//        return first.x < second.x
-//    }
-//    return first.y > second.y
-//}
-//var top = ""
-//var sides = ""
-//var bottom = ""
-//var y = rooms.first!.y
-//for room in rooms {
-//    if y != room.y {
-//        print(top)
-//        print(sides)
-//        print(bottom)
-//        top = ""
-//        sides = ""
-//        bottom = ""
-//        y = room.y
-//    }
-//
-//    top += "#" + (room.n == nil ? "#" : "-") + "#"
-//    sides += (room.w == nil ? "#" : "|") + "." + (room.e == nil ? "#" : "|")
-//    bottom += "#" + (room.s == nil ? "#" : "-") + "#"
-//}
-//print(top)
-//print(sides)
-//print(bottom)
+result = rooms.sorted { (first, second) -> Bool in
+    if first.value.y == second.value.y {
+        return first.value.x < second.value.x
+    }
+    return first.value.y > second.value.y
+}
+var top = ""
+var sides = ""
+var bottom = ""
+var y = rooms.first!.value.y
+for room in result {
+    if y != room.value.y {
+        print(top)
+        print(sides)
+        print(bottom)
+        top = ""
+        sides = ""
+        bottom = ""
+        y = room.value.y
+    }
+
+    top += "#" + (room.value.n == nil ? "#" : "-") + "#"
+    sides += (room.value.w == nil ? "#" : "|") + "." + (room.value.e == nil ? "#" : "|")
+    bottom += "#" + (room.value.s == nil ? "#" : "-") + "#"
+}
+print(top)
+print(sides)
+print(bottom)
